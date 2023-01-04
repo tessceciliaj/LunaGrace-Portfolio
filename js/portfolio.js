332ec0e85cb4f43ad27ac25e30e6634ed37589cb
Theresececilia <therese.johansson@edu.cmeducations.se>
2023-01-04 18:51:23 +0100

    contact page

--- js/portfolio.js
+++ js/portfolio.js
@@ -8,7 +8,7 @@ $(function () {
 });
 
 function setup() {
-  let cnv = createCanvas(190, 190);
+  let cnv = createCanvas(350, 350);
   let x = (windowWidth - width) / 2;
   let y = (windowHeight - height) / 2;
   cnv.position( x, y);
@@ -22,7 +22,7 @@ function draw() {
   rectMode(CENTER);
   translate(width / 2, height / 2);
   rotate(frameCount * 0.01);
-  square(0, 0, 200);
+  square(0, 0, 380);
 }
 
 
--- js/projects.js
+++ js/projects.js
@@ -11,4 +11,16 @@ const observer = new IntersectionObserver((entries) => {
   
   const hiddenElements = document.querySelectorAll(".hidden");
   hiddenElements.forEach((el) => observer.observe(el));
-  
+
+  document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+      
+      const rect = card.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+
+    }
+  }
